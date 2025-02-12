62b176543377fbb1d6f9de6498ff0249baf7b42d
Kozmosa <kozmosa@proton.me>
2025-02-12 14:09:03 +0800

    test passed

---
+++ build/CMakeFiles/account_book.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/account_book.dir/Account.cpp.o"
+  "CMakeFiles/account_book.dir/FileManager.cpp.o"
+  "CMakeFiles/account_book.dir/StatAggregator.cpp.o"
+  "CMakeFiles/account_book.dir/YearStat.cpp.o"
+  "CMakeFiles/account_book.dir/main.cpp.o"
+  "account_book"
+  "account_book.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/account_book.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
